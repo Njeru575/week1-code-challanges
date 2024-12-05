773a2660b4dc4c814420dc0712992c69c086b12e
emmanuel <emmanjeru2002@gmail.com>
2024-12-05 12:45:00 +0300

    final commit

--- challenge1.js
+++ challenge1.js
@@ -1,27 +1,30 @@
 function getGrade() {
   // Asks user to input marks
-  let marks = prompt("Enter student marks:");
-
+  let marks = prompt("Enter student marks (between 0 and 100):");
+  marks = Number(marks);
   // Checks if marks are between 0 and 100
-  if (marks >= 0 && marks <= 100) {
-    // Determines grades based on marks
-    let grade;
-    if (marks > 79) {
-      grade = "A";
-    } else if (marks >= 60) {
-      grade = "B";
-    } else if (marks >= 50) {
-      grade = "C";
-    } else if (marks >= 40) {
-      grade = "D";
-    } else {
-      grade = "E";
-    }
-    // Disaplays the grade
-    alert("The grade is:" + grade);
-  } else {
-    // If the input is not between 0 and 100
-    alert("Pleas enter a valid number between 0 and 100.");
+  if (marks < 0 || marks > 100 || isNaN(marks)) {
+    console.log("Invalid input. Please enter a number between 0 and 100.");
+    return;
   }
+  // Determine the grade based on marks
+  let grade;
+  if (marks > 79) {
+    grade = "A";
+  } else if (marks >= 60) {
+    grade = "B";
+  } else if (marks >= 50) {
+    grade = "C";
+  } else if (marks >= 40) {
+    grade = "D";
+  } else {
+    grade = "E";
+  }
+
+  // Output the grade
+  console.log(`The grade is: ${grade}`);
 }
-getGrade();
+
+
+// Call the function
+getStudentGrade();
